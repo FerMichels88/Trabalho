7d261b59a2752ae3873e6f5072be3a3f532c7d4a
fernando <michelsdc8@gmail.com>
2017-05-09 20:55:51 -0300

    deletando

---
+++ app/calculaTotalItem.js
@@ -0,0 +1,20 @@
+
+const calculaTotalItem = function(codigo, quantidade) {
+  const produtos = [
+   {id: 1, nome: 'Banana', valorUnitario: 2},
+   {id: 2, nome: 'Maça', valorUnitario: 5},
+   {id: 3, nome: 'Pera', valorUnitario: 6},
+   {id: 4, nome: 'Morango', valorUnitario: 1},
+  ];
+var produtoEncontrado;
+
+  for(var i = 0; i <= produtos.length-1; i++){
+    if (produtos[i].id == codigo) {
+      var produtoEncontrado= produtos[i];
+    }
+  }
+
+  return {produto: produtoEncontrado.nome, valor: produtoEncontrado.valorUnitario * quantidade };
+
+}
+module.exports = { calculaTotalItem };
